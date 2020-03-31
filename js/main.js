25694f3ceed082216fae4cce516097eef16f49a8
Salvatore Lauriola <salvatore6324@gmail.com>
2020-03-31 18:10:40 +0200

    Finished without css

--- js/main.js
+++ js/main.js
@@ -10,20 +10,30 @@ var cognomi = document.getElementById('cognomi');
 var listaCognomi = document.getElementById('lista-cognomi');
 var btn = document.getElementById('create');
 
+
 btn.addEventListener('click',
 
-function() {
-  list.push(cognomi.value);
-  list.sort();
+  function() {
+    if (cognomi.value == '') {
+        alert('Riempi il campo')
+        
+    } else if (!isNaN(cognomi.value)) {
+        alert('Carattere non disponibile')
+    
+      } else {
 
-  var items= '';
-  for (i=0; i < list.length; i++) {
-    items += '<li>' + list [i] + '</li>';
+        list.push(cognomi.value);
+        list.sort();
 
-    listaCognomi.innerHTML = items + 'Il tuo cognome è il ' + list.indexOf(cognomi.value);
-  }
-}
-);
+        var items= '';
+        for (var i=0; i < list.length; i++) {
+          items += '<li>' + list [i] + '</li>';
+        }
+        listaCognomi.innerHTML = items + 'Il tuo cognome è il ' + (list.indexOf(cognomi.value)+1);
+        cognomi.value = '';
+      }
+    } 
+  );
 
 
 
